4e09ab7829a14a186b4766f91a4225e086448aaf
aw4y <mobile@rehacktive.net>
2018-08-24 18:32:09 +0000

    fix sample

--- sample/main.go
+++ sample/main.go
@@ -9,8 +9,9 @@ import (
 
 func main() {
 
-	d := motion.New(motion.DefaultThresold, motion.DefaultSensitivity, motion.DefaultMinArea, "output.jpg")
-	detected, err := d.DetectMotion("/home/aw4y/motion_images/img/diff1/1.jpg", "/home/aw4y/motion_images/img/diff1/2.jpg")
+	d := motion.New(motion.DefaultThresold, motion.DefaultSensitivity, 10000, "output.jpg")
+	detected, err := d.DetectMotion("/ramfs/1.jpg", "/ramfs/2.jpg")
+
 	if err != nil {
 		fmt.Println("Error", err)
 		os.Exit(1)
